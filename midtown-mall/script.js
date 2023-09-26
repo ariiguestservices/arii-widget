d395e4e4085b173e955e1982461a0fa3b8230c25
imtiazraqib <imtiazud@ualberta.ca>
2023-09-26 09:53:11 -0600

    updated chat icon

--- midtown-mall/script.js
+++ midtown-mall/script.js
@@ -6,7 +6,7 @@ onclick="window.open(this.href, 'new', 'width=1280,height=720'); return false;"
 <div class="arii-button">
   <div class="flex-container">
     <div class="wrapper">
-      <img src="https://uploads-ssl.webflow.com/646e756c4526f6fa246bdca4/64f756c471356c6fdf7d6ebc_Chat-digital-customer-service.svg" alt="chat bubble" width="25px" />
+      <img src="https://uploads-ssl.webflow.com/646e756c4526f6fa246bdca4/6512fe2d457e6979d96becd4_chat-icon.png" alt="chat bubble" width="25px" />
     </div>
     <div class="wrapper">
       <p>Guest Experience</p>
