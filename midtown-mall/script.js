3b5af114cac84c7b9a08452c130e3b83133fcc45
Imtiaz Raqib <imtiazud@ualberta.ca>
2023-10-18 15:58:55 -0600

    allow links in iOS

--- midtown-mall/script.js
+++ midtown-mall/script.js
@@ -26,6 +26,7 @@ const iframeContainerDiv = `<div class="iframe-container hide">
   src="https://wecare.helloarii.com/e/midtowndemo"
   width="100%"
   height="100%"
+  sandbox="allow-popup"
   frameborder="0"></iframe>
 </div>`;
 
