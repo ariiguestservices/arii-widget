461ac9cd3338dfc6721a4c58982bc312b580d47d
Imtiaz Raqib <imtiazud@ualberta.ca>
2023-10-19 10:45:55 -0600

    reverted to seperate window

--- midtown-mall/script.js
+++ midtown-mall/script.js
@@ -1,5 +1,8 @@
-const widget = `<div
-id="arii">
+const widget = `<a
+id="arii"
+href="https://wecare.helloarii.com/e/midtowndemo"
+onclick="window.open(this.href, 'new', 'width=1280,height=720'); return false;"
+>
 <div class="arii-button">
   <div class="flex-container">
     <div class="wrapper">
@@ -11,49 +14,9 @@ id="arii">
     </div>
   </div>
 </div>
-</div>`;
+</a>`;
 
-const iframeContainerDiv = `<div class="iframe-container hide">
-<div class="iframe-c-top-bar">
-  <img
-    id="maximize"
-    src="https://uploads-ssl.webflow.com/646e756c4526f6fa246bdca4/65303f4b0db5313db40e986e_icons8-maximize-50.png" />
-  <img
-    id="close"
-    src="https://uploads-ssl.webflow.com/646e756c4526f6fa246bdca4/65303f4a4dc27cd6430b544c_icons8-close-50.png" />
-</div>
-<iframe
-  src="https://wecare.helloarii.com/e/midtowndemo"
-  width="100%"
-  height="100%"
-  frameborder="0"></iframe>
-</div>`;
-
+// Check and see if page is in iFrame
 if (window.location == window.parent.location) {
   document.body.innerHTML += widget;
-  document.body.innerHTML += iframeContainerDiv;
 }
-
-const widgetBtn = document.getElementById("arii");
-const iframeContainer = document.querySelector(".iframe-container");
-const maximizeBtn = document.getElementById("maximize");
-const closeBtn = document.getElementById("close");
-
-widgetBtn.addEventListener("click", () => {
-  iframeContainer.classList.remove("hide");
-  setTimeout(() => {
-    iframeContainer.classList.toggle("show-iframe");
-  }, 300);
-});
-
-closeBtn.addEventListener("click", () => {
-  iframeContainer.classList.toggle("show-iframe");
-  setTimeout(() => {
-    iframeContainer.classList.add("hide");
-    iframeContainer.classList.remove("maximize-iframe");
-  }, 100);
-});
-
-maximizeBtn.addEventListener("click", () => {
-  iframeContainer.classList.toggle("maximize-iframe");
-});
