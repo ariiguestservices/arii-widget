e1b11a892719b0a3c9c1386fe0f841f2308109ef
imtiazraqib <imtiazud@ualberta.ca>
2025-05-02 15:59:29 -0600

    added widget for website to vwdemo

---
+++ website/script.js
@@ -0,0 +1,22 @@
+const widget = `<a
+id="arii"
+href="https://wecare.helloarii.com/e/vwdemo"
+onclick="window.open(this.href, 'new', 'width=1280,height=720'); return false;"
+>
+<div class="arii-button">
+  <div class="flex-container">
+    <div class="wrapper">
+      <img src="https://cdn.prod.website-files.com/646e756c4526f6fa246bdca4/6474f49c8e3b2665bea7d61b_arii-webclip.png" alt="Arii Logo" width="60px" />
+    </div>
+    <div class="wrapper">
+      <p>Demo</p>
+      <p>Virtual Workshops</p>
+    </div>
+  </div>
+</div>
+</a>`;
+
+// Check and see if page is in iFrame
+if (window.location == window.parent.location) {
+  document.body.innerHTML += widget;
+}
